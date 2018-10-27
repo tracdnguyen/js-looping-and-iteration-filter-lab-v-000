8f5a32aa596e45055890aa830d29bb3ccd78ef2b
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-27 17:53:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// Code your solution in this file
-function findMatching (drivers, name) {
-  return drivers.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
-}
+
 function findMatching(drivers, name) {
   return drivers.filter(function (driverName) {
     return driverName === name
