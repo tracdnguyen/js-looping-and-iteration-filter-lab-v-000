30673c1c0260090f85ccc3a1531f648bbe3289b6
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-27 18:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,4 @@ function fuzzyMatch(drivers, subString) {
   })
 }
 
-function matchName(drivers, name) {
-  return drivers.filter(function(driverName) {
-    return driverName.name === name
-  })
-}
+function
